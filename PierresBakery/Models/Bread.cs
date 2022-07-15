6af7ae8cd4946d7f3f3a1b46054c0d4b2a8275ab
ftolentino <filmer.tolentino@gmail.com>
2022-07-15 14:22:58 -0700

    WIP BreadCost method

--- PierresBakery/Models/Bread.cs
+++ PierresBakery/Models/Bread.cs
@@ -5,11 +5,37 @@ namespace PierresBakery.Models
   public class Bread
   {
     // properties
-    public int Number { get; set; }
-    // constructor
-    public Bread(int number)
+    private static double _Price = 5;
+    public int Quantity { get; set; }
+
+    // Price property
+    public static double Price
     {
-      Number = number;
+      get { return _Price; }
+      set { _Price = value; }
+    }
+    // Bread constructor
+    public Bread(int quantity)
+    {
+      _Price = Price;
+      Quantity = quantity;
+    }
+
+    public double BreadCost()
+    {
+      double breadTotal = 0;
+      for (int index = 0; index <= Quantity; index++)
+      {
+        if (index == 0)
+        {
+          breadTotal += 0;
+        }
+        else
+        {
+          breadTotal += _Price;
+        }
+      }
+      return breadTotal;
     }
   }
 }
