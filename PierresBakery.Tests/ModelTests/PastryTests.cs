ad2d5d3b0eb5426048c82b10e944bf293c106673
ftolentino <filmer.tolentino@gmail.com>
2022-07-16 15:55:20 -0700

    Pass Get and Set Quantity test

--- PierresBakery.Tests/ModelTests/PastryTests.cs
+++ PierresBakery.Tests/ModelTests/PastryTests.cs
@@ -15,8 +15,28 @@ namespace PastryTests.Tests
     [TestMethod]
     public void PastryConstructor_CreatesInstanceOfPastry_Pastry()
     {
-      Pastry newPastry = new Pastry();
+      Pastry newPastry = new Pastry(1);
       Assert.AreEqual(typeof(Pastry), newPastry.GetType());
+    }
+
+    [TestMethod]
+    public void GetQuantity_ReturnQauntity_Quantity()
+    {
+      int quantity = 1;
+      Pastry newPastry = new Pastry(quantity);
+      int result = newPastry.Quantity;
+      Assert.AreEqual(quantity, result);
+    }
+
+    [TestMethod]
+    public void SetQuantity_SetQuantity_Quantity()
+    {
+      int quantity = 1;
+      Pastry newPastry = new Pastry(quantity);
+      int updatedQuantity = 2;
+      newPastry.Quantity = updatedQuantity;
+      int result = newPastry.Quantity;
+      Assert.AreEqual(updatedQuantity, result);
     }
   }
 }
