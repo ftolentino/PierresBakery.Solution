7d58a3cae0993220267ec554cbd7c1e0e345c590
ftolentino <filmer.tolentino@gmail.com>
2022-07-16 17:46:02 -0700

    Add _DealOne, _DealTwo properties and logic

--- PierresBakery/Models/Pastry.cs
+++ PierresBakery/Models/Pastry.cs
@@ -5,11 +5,57 @@ namespace PierresBakery.Models
   public class Pastry
   {
     // properties
+    private static double _Price = 2;
+    private double _DealOne;
+    private double _DealTwo;
     public int Quantity { get; set; }
+
+    // Price property
+    public static double Price
+    {
+      get { return _Price; }
+      set { _Price = value; }
+    }
+
     // Pastry constructor
     public Pastry(int quantity)
     {
+      _Price = Price;
+      _DealOne = 1;
+      _DealTwo = 2;
       Quantity = quantity;
+    }
+
+    public double PastryCost()
+    {
+      double pastryTotal = 0;
+      for (int index = 0; index <= Quantity; index++)
+      {
+        if (index == 0)
+        {
+          pastryTotal += 0;
+        }
+        else if (index % 3 == 0)
+        {
+          pastryTotal += _Price - _DealOne;
+        }
+        else if (index % 6 == 0)
+        {
+          pastryTotal += _Price - _DealTwo;
+        }
+        else
+        {
+          pastryTotal += _Price;
+        }
+      }
+      return pastryTotal;
     }
   }
 }
+
+// Pastry Discount
+// 1 = $2
+// 3 = $5 $1 off
+// 4 = 7 $1 off
+// 5 = 9 $1 off
+// 6 = 10 $2 off
--- PierresBakery/Program.cs
+++ PierresBakery/Program.cs
@@ -14,10 +14,16 @@ namespace PierresBakery.Models
     Console.WriteLine("For every 2 loaves of bread, you get the 3rd free!");
     Console.WriteLine("Pastry Deals: 3 pastries for $5, 4 pastries for $7, 5 pastries for $9, 6 pastries for $10");
     Console.WriteLine("Enter how many loaves of bread you would like to purchase");
-    
+
     int breadOrder = int.Parse(Console.ReadLine());
     Bread userBread = new Bread(breadOrder);
     Console.WriteLine(userBread.BreadCost());
+
+    Console.WriteLine("Enter how many pastries you would like to purchase");
+
+    int pastryOrder = int.Parse(Console.ReadLine());
+    Pastry userPastry = new Pastry(pastryOrder);
+    Console.WriteLine(userPastry.PastryCost());
   }
   }
 }
